15b244c25f417a32fd68705026e09a0cc9d8950f
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-02 14:07:02 +0800

    initial sql migration files

---
+++ pkg/db/migrations/000001_init.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS persons_family;
+DROP TABLE IF EXISTS user_networks;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS families;
+DROP TABLE IF EXISTS social_networks;
+DROP TABLE IF EXISTS roles;
+
+DROP TYPE IF EXISTS gender_enum;
+DROP TYPE IF EXISTS family_role_enum;
---
+++ pkg/db/migrations/000001_init.up.sql
@@ -0,0 +1,49 @@
+CREATE TYPE gender_enum AS ENUM ('Male', 'Female');
+CREATE TYPE family_role_enum AS ENUM ('Father', 'Mother', 'Child');
+
+CREATE TABLE IF NOT EXISTS roles (
+	id serial PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	description VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS social_networks (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	base_url VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS families (
+	id SERIAL PRIMARY KEY,
+	family_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS persons (
+	uid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
+	middle_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	gender gender_enum NOT NULL,
+	birthday DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	uid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	role_id INTEGER REFERENCES roles(id) NOT NULL,
+	persons_id UUID REFERENCES persons(uid) UNIQUE,
+	email VARCHAR(100) UNIQUE,
+	password BYTEA NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_networks (
+	id SERIAL PRIMARY KEY,
+	user_uid UUID REFERENCES users(uid) NOT NULL,
+	social_networks_id INTEGER REFERENCES social_networks(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS persons_family (
+	id SERIAL PRIMARY KEY,
+	family_id INTEGER REFERENCES families(id) NOT NULL,
+	person_id UUID REFERENCES persons(uid) NOT NULL,
+	family_role family_role_enum NOT NULL
+);
