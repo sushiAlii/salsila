3e61bc634a8488cb42bea2d050711469e09d61ba
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-08 22:25:29 +0800

    Implemented jwt tokens

--- pkg/controllers/auth.go
+++ pkg/controllers/auth.go
@@ -8,6 +8,42 @@ import (
 	"github.com/sushiAlii/salsila/pkg/models"
 )
 
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	var credentials struct {
+		Email		string	`json:"email"`
+		Password	string	`json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := models.LoginUser(db.DB, credentials.Email, credentials.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	token, err := models.CreateToken(user.UID)
+	if err != nil {
+		http.Error(w, "Failed to create a token", http.StatusInternalServerError)
+		return
+	}
+
+	if err := models.SaveAuth(db.DB, user.UID, token); err != nil {
+		http.Error(w, "Failed to save authentication token", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"access_token": token.AccessToken,
+		"refresh_token": token.RefreshToken,
+	})
+}
+
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var newUser models.User
 
@@ -30,5 +66,26 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "Successfully created a User!",
+	})
+}
+
+func RefreshToken(w http.ResponseWriter, r *http.Request) {
+	refreshToken := r.Header.Get("Refresh-Token")
+
+	if refreshToken == "" {
+		http.Error(w, "Refresh token does not exist", http.StatusBadRequest)
+		return
+	}
+
+	newToken, err := models.Refresh(db.DB, refreshToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"access_token": newToken.AccessToken,
+		"refresh_token": newToken.RefreshToken,
 	})
 }
--- pkg/models/auth.go
+++ pkg/models/auth.go
@@ -1,15 +1,42 @@
 package models
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
-func LoginUser(DB *gorm.DB, email, password string) (*User, error) {
-	var user User
+var (
+	accessSecret = os.Getenv("ACCESS_SECRET")
+	refreshSecret = os.Getenv("REFESH_SECRET")
+)
 
-	_, err := GetUsersByEmail(DB, email)
+type TokenDetails struct {
+	AccessToken 	string
+	RefreshToken	string
+	ATExpires		int64
+	RTExpires		int64
+}
+
+type RefreshToken struct {
+	ID			uint		`gorm:"primaryKey"`
+	UserUID		string		`gorm:"type:uuid;not null"`
+	Token		string		`gorm:"not null"`
+	ExpiresAt	time.Time	`gorm:"type:timestamptz;not null"`
+	CreatedAt	time.Time	`gorm:"type:timestamptz;not null"`
+}	
+
+func LoginUser(DB *gorm.DB, email, password string) (*User, error) {
+	user, err := GetUserByEmail(DB, email)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrUserNotFound
+		}
 		return nil, err
 	}
 
@@ -17,9 +44,120 @@ func LoginUser(DB *gorm.DB, email, password string) (*User, error) {
 		return nil, ErrPasswordIncorrect
 	}
 
-	return &user, nil
+	return user, nil
 }
 
 func RegisterUser(DB *gorm.DB, user *User) error {
 	return CreateUser(DB, user)
+}
+
+func CreateToken(userUID string) (*TokenDetails, error) {
+	token := &TokenDetails{}
+
+	token.ATExpires = time.Now().Add(time.Minute * 15).Unix()
+	token.RTExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+
+	var err error
+
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_uid"] = userUID
+	atClaims["exp"] = token.ATExpires
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token.AccessToken, err = accessToken.SignedString([]byte(accessSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	rtClaims := jwt.MapClaims{}
+	rtClaims["user_uid"] = userUID
+	rtClaims["exp"] = token.RTExpires
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	token.RefreshToken, err = refreshToken.SignedString([]byte(refreshSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func SaveAuth(DB *gorm.DB, userUid string, td *TokenDetails) error {
+	token := &RefreshToken{
+		Token:		td.RefreshToken,
+		UserUID: 	userUid,
+		ExpiresAt: 	time.Unix(td.RTExpires, 0),
+	}
+
+	return DB.Create(token).Error;
+}
+
+func Refresh(DB *gorm.DB, refreshToken string) (*TokenDetails, error) {
+	tx := DB.Begin()
+
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		if _,ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(refreshSecret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("Token is not valid")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("Invalid token")
+	}
+
+	userUid, ok := claims["user_uid"].(string)
+	if !ok {
+		return nil, fmt.Errorf("Invalid claim: %s", userUid)
+	}
+
+	if _, err := VerifyToken(DB, userUid, refreshToken); err != nil {
+		return nil, err
+	}
+
+	newToken, err := CreateToken(userUid)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := SaveAuth(DB, userUid, newToken); err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return nil, err
+	}
+
+	return newToken, nil
+}
+
+func VerifyToken(DB *gorm.DB, userUid string, tokenString string) (*RefreshToken, error) {
+	var token RefreshToken
+
+	if err := DB.Where("user_uid = ? AND token = ?", userUid, tokenString).First(&token).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("Token not found")
+		}
+		return nil, err
+	}
+
+	if token.ExpiresAt.Before(time.Now()) {
+		return nil, fmt.Errorf("Token has expired")
+	}
+
+	if err := DB.Where("user_uid ? AND token = ?", userUid, tokenString).Delete(&RefreshToken{}).Error; err != nil {
+		return nil, fmt.Errorf("Unable to refresh token: %v", err)
+	}
+
+	return &token, nil
 }
