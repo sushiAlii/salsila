a92073768bd7c2ccbfc9045a3ddf5627d3e2af17
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-08 22:26:40 +0800

    Added register login and refresh auth routes

--- pkg/routes/auth.go
+++ pkg/routes/auth.go
@@ -8,5 +8,12 @@ import (
 func ConfigureAuthRoutes(r *mux.Router) {
 	router := r.PathPrefix("/auth").Subrouter()
 
+	//	Register
 	router.HandleFunc("/register", controllers.RegisterUser).Methods("POST")
+
+	//	Login
+	router.HandleFunc("/login", controllers.LoginUser).Methods("POST")
+
+	//	Refresh token
+	router.HandleFunc("/refresh", controllers.RefreshToken).Methods("POST")
 }
