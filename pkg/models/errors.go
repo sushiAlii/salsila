4be1036a2406f7c3eac6c444deddc052520d04fb
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-12 00:10:02 +0800

    Added errors for person attributes

--- pkg/models/errors.go
+++ pkg/models/errors.go
@@ -10,4 +10,11 @@ var (
 	ErrPasswordRequired 	= 	errors.New("Password is required")
 	ErrPasswordMinChar 		= 	errors.New("Password should have more than 4 characters")
 	ErrPasswordIncorrect 	= 	errors.New("Incorrect Password!")
+
+	ErrFirstNameRequired	=	errors.New("First name is required")
+	ErrMiddleNameRequired	= 	errors.New("Middle name is required")
+	ErrLastNameRequired		=	errors.New("Last name is required")
+	ErrBirthdayInvalid		=	errors.New("Invalid birthday")
+	ErrGenderRequired		=	errors.New("Gender is required")
+	ErrGenderInvalid		=	errors.New("Gender should only be on specified options")
 )
