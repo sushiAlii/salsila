0c6bf1822566282b676f730e7342e7d122aa1629
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-12 01:56:43 +0800

    Added CRUD function inside user_networks.go model

--- pkg/models/user_networks.go
+++ pkg/models/user_networks.go
@@ -1,12 +1,77 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 
 type UserNetwork struct {
-	ID				uint		`gorm:"primaryKey"`
-	UserUID			string		`gorm:"not null"`
-	SocialNetworkID	uint		`gorm:"not null"`
-	CreatedAt		time.Time	`gorm:"type:timestamptz"`
-	UpdatedAt		*time.Time	`gorm:"type:timestamptz"`
+	ID				uint		`gorm:"primaryKey" json:"id"`
+	UserUID			string		`gorm:"not null" json:"userUid"`
+	SocialNetworkID	uint		`gorm:"not null" json:"socialNetworkId"`
+	UserName		string		`json:"userName"`
+	UserURL			string		`gorm:"not null" json:"userUrl"`
+	CreatedAt		time.Time	`gorm:"type:timestamptz" json:"-"`
+	UpdatedAt		*time.Time	`gorm:"type:timestamptz" json:"-"`
+}
+
+
+func CreateUserNetwork(DB *gorm.DB, userNetwork *UserNetwork) error {
+	return DB.Create(userNetwork).Error
+}
+
+func GetAllUserNetworks(DB *gorm.DB) ([]UserNetwork, error) {
+	var userNetworks []UserNetwork
+
+	if err := DB.Find(&userNetworks).Error; err != nil {
+		return nil, err
+	}
+
+	return userNetworks, nil
+}
+
+func GetUserNetworksByUserUID(DB *gorm.DB, userUid string) ([]UserNetwork, error) {
+	var userNetworks []UserNetwork
+
+	if err := DB.Find(&userNetworks, "user_uid = ?", userUid).Error; err != nil {
+		return nil, err
+	}
+
+	return userNetworks, nil
+}
+
+func GetUserNetworkByID(DB *gorm.DB, id uint) (*UserNetwork, error) {
+	var userNetwork UserNetwork
+
+	if err := DB.First(&userNetwork, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &userNetwork, nil
+}
+
+func UpdateUserNetworkByID(DB *gorm.DB, userNetwork *UserNetwork, id uint) error {
+	tx := DB.Begin()
+
+	if err := tx.Model(&UserNetwork{}).Where("id = ?", id).Updates(&userNetwork).Error; err != nil {
+		tx.Rollback()
+
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+func DeleteUserNetworkByID(DB *gorm.DB, id uint) error {
+	tx := DB.Begin()
+
+	if err := tx.Delete(&UserNetwork{}, id).Error; err != nil {
+		tx.Rollback()
+
+		return err
+	}
+
+	return tx.Commit().Error
 }
