b1b2cbc3159b1805444743b3bddcd00b51bc386e
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-11-12 01:52:26 +0800

    Added comment to arrange routes for socials.go route

--- pkg/routes/socials.go
+++ pkg/routes/socials.go
@@ -8,11 +8,16 @@ import (
 func ConfigureSocialNetworkRoutes(r *mux.Router) {
 	router := r.PathPrefix("/social_networks").Subrouter()
 
+	//	Create
+	router.HandleFunc("", controllers.CreateSocialNetwork).Methods("POST")
+
+	//	Read
 	router.HandleFunc("", controllers.GetAllSocialNetworks).Methods("GET")
 	router.HandleFunc("/{id:[0-9]+}", controllers.GetSocialNetworkByID).Methods("GET")
 
-	router.HandleFunc("", controllers.CreateSocialNetwork).Methods("POST")
+	//	Update
 	router.HandleFunc("/{id:[0-9]+}", controllers.UpdateSocialNetworkByID).Methods("PATCH")
 
+	//	Delete
 	router.HandleFunc("/{id:[0-9]+}", controllers.DeleteSocialNetworkByID).Methods("DELETE")
 }
