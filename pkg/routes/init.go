ad622816b990d3124e6f17f49cc7cc6e64c12c1f
Ali Laut <ali.laut@rakutentravelxchange.com>
2023-12-12 22:37:03 +0800

    Refactors - Used controllers struct as parameter on ConfigureAllRoutes

--- pkg/routes/init.go
+++ pkg/routes/init.go
@@ -5,12 +5,12 @@ import (
 	"github.com/sushiAlii/salsila/pkg/controllers"
 )
 
-func ConfigureAllRoutes(r *mux.Router, roleController *controllers.RoleController, socialNetworkController *controllers.SocialNetworkController, familyController *controllers.FamilyController, userController *controllers.UserController, authController *controllers.AuthController, personController *controllers.PersonController, userNetworkController *controllers.UserNetworkController) {
-	ConfigureRoleRoutes(r, roleController)
-	ConfigureSocialNetworkRoutes(r, socialNetworkController)
-	ConfigureFamilyRoutes(r, familyController)
-	ConfigureUserRoutes(r, userController)
-	ConfigureAuthRoutes(r, authController)
-	ConfigurePersonRoutes(r, personController)
-	ConfigureUserNetworkRoutes(r, userNetworkController)
+func ConfigureAllRoutes(r *mux.Router, c controllers.Controllers) {
+	ConfigureRoleRoutes(r, c.RoleController)
+	ConfigureSocialNetworkRoutes(r, c.SocialNetworkController)
+	ConfigureFamilyRoutes(r, c.FamilyController)
+	ConfigureUserRoutes(r, c.UserController)
+	ConfigureAuthRoutes(r, c.AuthController)
+	ConfigurePersonRoutes(r, c.PersonController)
+	ConfigureUserNetworkRoutes(r, c.UserNetworkController)
 }
